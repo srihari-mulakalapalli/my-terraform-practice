10c4df555287468331e097b2ffe92b52966b89eb
srihari-mulakalapalli <srihari25.mulakalapalli@gmail.com>
2024-09-26 12:08:45 +0530

    created DynaboDB table

--- Day-4/main.tf
+++ Day-4/main.tf
@@ -11,3 +11,14 @@ resource "aws_instance" "srihari" {
 resource "aws_s3_bucket" "s3_bucket" {
     bucket = "srihari-s3-demo"  
 }
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
