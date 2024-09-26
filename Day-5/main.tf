3ef4f68f53bedf0efd839971b1c6b7b05aa90f5c
srihari-mulakalapalli <srihari25.mulakalapalli@gmail.com>
2024-09-26 16:52:30 +0530

    Created EC2 instance resources

--- Day-5/main.tf
+++ Day-5/main.tf
@@ -1,24 +1,25 @@
+# Define the AWS provider configuration.
 provider "aws" {
-    region = "us-east-1"  
+  region = "us-east-1"  
 }
 
 variable "cidr" {
-    default = "10.80.0.0/16"
+  default = "10.80.0.0/16"
 }
 
 resource "aws_key_pair" "example" {
-    key_name = "Laptop_key"
-    public_key = file("~/.ssh/id_rsa.pub")
+  key_name   = "Laptop_key" 
+  public_key = file("~/.ssh/id_rsa.pub") 
 }
 
 resource "aws_vpc" "myvpc" {
   cidr_block = var.cidr
 }
 
-resource "aws_subnet" "pub_subnet1" {
-  vpc_id = aws_vpc.myvpc
-  cidr_block = "10.80.0.0/24"
-  availability_zone = us-east-1
+resource "aws_subnet" "sub1" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.80.0.0/24"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
@@ -36,33 +37,33 @@ resource "aws_route_table" "RT" {
 }
 
 resource "aws_route_table_association" "rta1" {
-  subnet_id = aws_subnet.pub_subnet1
+  subnet_id      = aws_subnet.sub1.id
   route_table_id = aws_route_table.RT.id
 }
 
 resource "aws_security_group" "webSg" {
-  name = "web"
+  name   = "web"
   vpc_id = aws_vpc.myvpc.id
 
   ingress {
     description = "HTTP from VPC"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/."]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description = "SSH"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = -1
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -72,17 +73,17 @@ resource "aws_security_group" "webSg" {
 }
 
 resource "aws_instance" "server" {
-  ami = "ami-0e86e20dae9224db8"
-  instance_type = "t2.micro"
-  key_name = aws_key_pair.example.key_name
+  ami                    = "ami-0e86e20dae9224db8"
+  instance_type          = "t2.micro"
+  key_name      = aws_key_pair.example.key_name
   vpc_security_group_ids = [aws_security_group.webSg.id]
-  subnet_id = aws_subnet.pub_subnet1
+  subnet_id              = aws_subnet.sub1.id
 
   connection {
-    type = "ssh"
-    user = "ubuntu"
-    private_key = file("~/.ssh/id_rsa")
-    host = self.public_ip
+    type        = "ssh"
+    user        = "ubuntu"  
+    private_key = file("~/.ssh/id_rsa") 
+    host        = self.public_ip
   }
 
   # File provisioner to copy a file from local to the remote EC2 instance
@@ -101,12 +102,4 @@ resource "aws_instance" "server" {
       "sudo python3 app.py &",
     ]
   }
-}  
-
-
-
-     
-  
-
-
-
+}
