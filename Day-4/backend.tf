342ea812ab2447bfaac09acd9433d2b5167a27a2
srihari-mulakalapalli <srihari25.mulakalapalli@gmail.com>
2024-09-26 12:14:06 +0530

    Nothing

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket         = "srihari-s3-demo" # change this
     key            = "srihari/terraform.tfstate"
     region         = "us-east-1"
-  }
+    encrypt        = true
+    dynamodb_table = "terraform_lock"
+    }
 }
