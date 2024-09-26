8ebe2eeb866f69f16a4b2b0ac0f6f10fa229c747
srihari-mulakalapalli <srihari25.mulakalapalli@gmail.com>
2024-09-26 17:23:27 +0530

    Added python filess

--- Day-5/main.tf
+++ Day-5/main.tf
@@ -88,7 +88,7 @@ resource "aws_instance" "server" {
 
   # File provisioner to copy a file from local to the remote EC2 instance
   provisioner "file" {
-    source      = "D:\My-Terraform-Code\app.py"  # Replace with the path to your local file
+    source      = "C:/Users/HP/bin/app.py"  # Replace with the path to your local file
     destination = "/home/ubuntu/app.py"  # Replace with the path on the remote instance
   }
 
